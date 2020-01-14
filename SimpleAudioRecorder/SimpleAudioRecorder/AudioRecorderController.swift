6dc54c7da9013f69a726426f57415fcf43f9ffec
Percy <percival.ngan@gmail.com>
2020-01-14 11:32:31 -0600

    Play button is working

--- SimpleAudioRecorder/SimpleAudioRecorder/AudioRecorderController.swift
+++ SimpleAudioRecorder/SimpleAudioRecorder/AudioRecorderController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import AVFoundation // 1. for audio players
 
 class AudioRecorderController: UIViewController {
-    
+
+	var audioPlayer: AVAudioPlayer? // 2
+	var isPlaying: Bool { // 6
+		audioPlayer?.isPlaying ?? false
+	}
+
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var timeLabel: UILabel!
@@ -29,16 +35,56 @@ class AudioRecorderController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
-
+		// monospace is good for timers because all the txt have the same spacing
         timeLabel.font = UIFont.monospacedDigitSystemFont(ofSize: timeLabel.font.pointSize,
                                                           weight: .regular)
         timeRemainingLabel.font = UIFont.monospacedDigitSystemFont(ofSize: timeRemainingLabel.font.pointSize,
                                                                    weight: .regular)
+
+		loadAudio() // 5
 	}
+
+	// Playback APIs
+
+	// What are the function names I need?
+
+	// - get audio file
+	// - play
+	// - pause
+	// - timestamp
+	// - is it playing?
+
+	private func loadAudio() {
+		// piano.mp3
+		// App Bundle
+		// Documents - readwrite
+
+		let songURL = Bundle.main.url(forResource: "piano", withExtension: "mp3")! // 3. Force unwrapping
+
+		audioPlayer = try! AVAudioPlayer(contentsOf: songURL) // 4. // FIXME: catch error and print
+	}
+
+	func play() { // 7
+		audioPlayer?.play()
+	}
+
+	func pause() { // 8
+		audioPlayer?.pause()
+	}
+
+	func playPause() { // 9
+		if isPlaying {
+			pause()
+		} else {
+			play()
+		}
+	}
+
+	// Record APIs
 
 
     @IBAction func playButtonPressed(_ sender: Any) {
-
+		playPause() // 10
 	}
     
     @IBAction func recordButtonPressed(_ sender: Any) {
